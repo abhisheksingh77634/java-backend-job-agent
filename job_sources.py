9ec1684978f1553ff5199d2b043a04e619384ad0
abhisheksingh77634 <71337175+abhisheksingh77634@users.noreply.github.com>
2026-02-07 20:15:43 +0530

    Add job sources for multiple countries

---
+++ job_sources.py
@@ -0,0 +1,17 @@
+JOB_SOURCES = {
+   "Germany": [
+       "https://www.linkedin.com/jobs/search/?keywords=Java%20Backend%20Visa%20Germany"
+   ],
+   "UK": [
+       "https://www.linkedin.com/jobs/search/?keywords=Java%20Backend%20Visa%20UK"
+   ],
+   "UAE": [
+       "https://www.linkedin.com/jobs/search/?keywords=Java%20Backend%20Dubai"
+   ],
+   "Singapore": [
+       "https://www.linkedin.com/jobs/search/?keywords=Java%20Backend%20Singapore%20EP"
+   ],
+   "Australia": [
+       "https://www.linkedin.com/jobs/search/?keywords=Java%20Backend%20Visa%20Australia"
+   ]
+}
